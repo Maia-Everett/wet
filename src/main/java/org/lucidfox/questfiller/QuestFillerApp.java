9000b1c72e0ee0372f43cbe71b0d7de8f387a6a0
Maia Everett <maia@lucidfox.org>
2016-10-14 16:50:03 +0700

    Remember window size and maximized state between launches

--- src/main/java/org/lucidfox/questfiller/QuestFillerApp.java
+++ src/main/java/org/lucidfox/questfiller/QuestFillerApp.java
@@ -2,6 +2,7 @@ package org.lucidfox.questfiller;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.prefs.Preferences;
 
 import org.lucidfox.questfiller.controller.AppController;
 import org.lucidfox.questfiller.ui.MainWindow;
@@ -16,6 +17,22 @@ public class QuestFillerApp extends Application {
 		for (final String fontName : Arrays.asList("NotoSans-Regular", "UbuntuMono-R")) {
 			Font.loadFont(MainWindow.class.getResource("fonts/" + fontName + ".ttf").toExternalForm(), 0);
 		}
+		
+		// Load stored window size
+		final Preferences prefs = Preferences.userNodeForPackage(QuestFillerApp.class);
+		final boolean maximized = prefs.getBoolean("windowMaximized", false);
+		final int width = prefs.getInt("windowWidth", 900);
+		final int height = prefs.getInt("windowHeight", 600);
+		
+		primaryStage.setWidth(width);
+		primaryStage.setHeight(height);
+		primaryStage.setMaximized(maximized);
+		primaryStage.setOnHiding(e -> {
+			// Save new window size
+			prefs.putBoolean("windowMaximized", primaryStage.isMaximized());
+			prefs.putInt("windowWidth", (int) primaryStage.getWidth());
+			prefs.putInt("windowHeight", (int) primaryStage.getHeight());
+		});
 		
 		final AppController appController = new AppController(primaryStage);
 		appController.showUI();
