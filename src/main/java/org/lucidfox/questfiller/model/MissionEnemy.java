767f531365938fefaae50d1d21fc524fda74dddb
Maia Everett <maia@lucidfox.org>
2016-11-19 23:39:59 +0700

    Allow mission enemies to have multiple counters

--- src/main/java/org/lucidfox/questfiller/model/MissionEnemy.java
+++ src/main/java/org/lucidfox/questfiller/model/MissionEnemy.java
@@ -1,19 +1,27 @@
 package org.lucidfox.questfiller.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public final class MissionEnemy {
 	private final String name;
-	private final String counter;
+	private final List<String> counters;
 
-	public MissionEnemy(final String name, final String counter) {
+	public MissionEnemy(final String name, final Iterable<String> counters) {
 		this.name = name;
-		this.counter = counter;
+		
+		final ArrayList<String> countersCopy = new ArrayList<>();
+		counters.forEach(countersCopy::add);
+		countersCopy.trimToSize();
+		this.counters = Collections.unmodifiableList(countersCopy);
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public String getCounter() {
-		return counter;
+	public List<String> getCounters() {
+		return counters;
 	}
 }
