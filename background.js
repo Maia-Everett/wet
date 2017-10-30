ad76843f2bae4223d9f43e7e9d6025e73c9db40d
Maia Everett <maia@lucidfox.org>
2017-10-30 20:03:56 +0700

    Eliminate popup window and open popup on wowhead page instead

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-let lastResult = null;
-let popupTab = null;
-let browserInfo = null;
-
-if (typeof browser !== "undefined") {
-	browser.runtime.getBrowserInfo().then(info => browserInfo = info);
-}
-
 function isSupported(url) {
 	let result = /https?:\/\/(www\.)?wowhead.com\/(quest|mission|npc)=/.test(url);
 	return result;
@@ -22,14 +14,6 @@ chrome.tabs.onActivated.addListener(activeInfo => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	if (popupTab !== null && tabId == popupTab.id) {
-		if (changeInfo.status == "complete") {
-			chrome.tabs.sendMessage(popupTab.id, { result: lastResult });
-		}
-
-		return;
-	}
-
 	if (isSupported(tab.url)) {
 		chrome.pageAction.show(tabId);
 	} else {
@@ -38,23 +22,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 chrome.pageAction.onClicked.addListener(tab => {
-	chrome.tabs.executeScript(tab.id, {
-		file: "/page/page.js"
-	}, result => {
-		lastResult = result;
-
-		// Work around Firefox 57 beta bug with blank popup window
-		var isFirefox57 = browserInfo
-			&& browserInfo.name === "Firefox"
-			&& +browserInfo.version >= 57;
-
-		chrome.windows.create({
-			width: 800,
-			height: 600,
-			type: isFirefox57 ? null : "popup",
-			url: "/result/result.html"
-		}, window => {
-			popupTab = window.tabs[0];
+	chrome.tabs.insertCSS(tab.id, {
+		file: "/page/page.css"
+	}, function() {
+		chrome.tabs.executeScript(tab.id, {
+			file: "/page/page.js"
 		});
 	});
 });
--- page/page.js
+++ page/page.js
@@ -3,8 +3,25 @@
 	let $ = document.querySelector.bind(document);
 	let $$ = document.querySelectorAll.bind(document);
 
-	// Test
-	$("#main-precontents").style.display = "none";
+	$$("#questfiller-popup").forEach(element => element.parentElement.removeChild(element));
 
-	return "Hello World!";
+	let popup = document.createElement("div");
+	popup.setAttribute("id", "questfiller-popup");
+	document.body.appendChild(popup);
+
+	popup.innerHTML = `
+		<textarea id="questfiller-content"></textarea>
+		<button id="questfiller-copy">Copy to clipboard and close</button>
+	`;
+
+	var content = $("#questfiller-content");
+
+	$("#questfiller-copy").addEventListener("click", e => {
+		content.select();
+		document.execCommand("copy");
+		popup.parentElement.removeChild(popup);
+	});
+
+	content.value = "Hello World!";
+	$("#questfiller-copy").focus();
 })();
