7231779089d9c37a65fb5a9c9d91e10d39fc2455
Maia Everett <maia@everett.one>
2020-05-13 17:38:52 +0700

    Add dark theme

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ document.body.appendChild(popup);
 
 popup.innerHTML = `
 	<textarea id="questfiller-content" spellcheck="false"></textarea>
+	<a id="questfiller-dark-toggle" href="#">Use dark theme</a>
 	<button id="questfiller-copy">Copy to clipboard and close</button>
 	<button id="questfiller-close">Close</button>
 `;
@@ -26,6 +27,34 @@ $("#questfiller-copy").addEventListener("click", e => {
 $("#questfiller-close").addEventListener("click", e => {
 	popup.parentElement.removeChild(popup);
 });
+
+// Dark theme support
+
+let darkThemeToggle = $("#questfiller-dark-toggle");
+let isDark = false;
+
+function setTheme() {
+	if (isDark) {
+		popup.classList.add("questfiller-dark");
+		darkThemeToggle.textContent = "Use light theme";
+	} else {
+		popup.classList.remove("questfiller-dark");
+		darkThemeToggle.textContent = "Use dark theme";
+	}
+}
+
+chrome.storage.sync.get(["isDark"], result => {
+	isDark = result.isDark || false;
+	setTheme();
+});
+
+darkThemeToggle.addEventListener("click", () => {
+	isDark = !isDark;
+	chrome.storage.sync.set({ "isDark": isDark });
+	setTheme();
+});
+
+// End dark theme support
 
 Parsers.create().then(parsers => {
 	let value = parsers.format();
